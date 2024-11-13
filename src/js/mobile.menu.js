ece2f159ed051e46ac106de15cca8a8e5b53c799
vini-j-pereira <viniciusjosepereira@gmail.com>
2024-11-12 22:39:36 -0300

    feat: navLinks fade

--- src/js/mobile.menu.js
+++ src/js/mobile.menu.js
@@ -8,8 +8,17 @@ class MobileNavbar{
         this.handleClick = this.handleClick.bind(this);
     }
 
+    animateLinks(){
+        this.navLinks.forEach((link) => {
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`)
+        });
+    }
+
     handleClick(){
         this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
     }
 
 
